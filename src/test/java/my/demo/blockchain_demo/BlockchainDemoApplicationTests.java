9cc635244d512dd641a76a7590a3432aaeaf1b7b
sergei.lyuty <sergei.lyuty@libertexgroup.com>
2023-09-26 16:55:00 +0200

    start

---
+++ src/test/java/my/demo/blockchain_demo/BlockchainDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package my.demo.blockchain_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
